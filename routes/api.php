6cddbf07d80985c235b4438c5aaf6f8ecdc73bf6
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2024-07-22 16:01:58 -0500

    Consulta de datos: rutas y controladores

---
+++ routes/api.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\RecipeController;
+use App\Http\Controllers\Api\TagController;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+
+Route::get('categories', [CategoryController::class, 'index']);
+Route::get('categories/{category}', [CategoryController::class, 'show']);
+
+Route::get('recipes', [RecipeController::class, 'index']);
+Route::get('recipes/{recipe}', [RecipeController::class, 'show']);
+
+Route::get('tags', [TagController::class, 'index']);
+Route::get('tags/{tag}', [TagController::class, 'show']);
