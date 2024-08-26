3a615025e15cbc89971f14de783cf69bd2a65bb9
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2024-08-26 15:35:59 -0500

    validacion de datos

--- app/Http/Controllers/Api/RecipeController.php
+++ app/Http/Controllers/Api/RecipeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreRecipeRequest;
+use App\Http\Requests\UpdateRecipeRequest;
 use App\Http\Resources\RecipeResource;
 use App\Models\Recipe;
 use Illuminate\Http\Request;
@@ -23,24 +25,24 @@ class RecipeController extends Controller
         return new RecipeResource($recipes);
     }
 
-    public function store(Request $request)
+    public function store(StoreRecipeRequest $request)
     {
 
         $recipe = Recipe::create($request->all());
-
         if($tags = json_decode($request->tags)) {
             $recipe->tags()->attach($tags);
         }
+
         return response()->json(new RecipeResource($recipe), Response::HTTP_CREATED);
     }
 
-    public function update(Request $request, Recipe $recipe)
+    public function update(UpdateRecipeRequest $request, Recipe $recipe)
     {
         //
         $recipe->update($request->all());
 
         if($tags = json_decode($request->tags)) {
-            $recipe->tags()->async($tags);
+            $recipe->tags()->sync($tags);
         }
         return response()->json(new RecipeResource($recipe), Response::HTTP_OK);
     }
